7ca903f7bceb8547d7075822b32ca1258f570cae
Ben <tsun424@163.com>
2017-11-21 21:42:47 +1300

    refactoring notification util

--- app/src/main/java/site/tsun/kotlinexamples/common/notification/NotificationActivity.kt
+++ app/src/main/java/site/tsun/kotlinexamples/common/notification/NotificationActivity.kt
@@ -8,7 +8,6 @@ import android.support.v7.app.AppCompatActivity
 import android.view.View
 import kotlinx.android.synthetic.main.activity_notification.*
 import site.tsun.kotlinexamples.R
-import site.tsun.kotlinexamples.util.NotificationUtil
 
 
 class NotificationActivity : AppCompatActivity(), View.OnClickListener {
